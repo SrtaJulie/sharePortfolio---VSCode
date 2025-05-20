d4010bc1b9d511839369b2cfa84fc6e73e99054f
henry <chloehenry1.ch@gmail.com>
2025-05-20 16:46:00 +0200

    test de la Map [US]: Création d’action simple #14

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -1,6 +1,8 @@
 package fr.utc.miage.shares;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,15 +13,15 @@ class ActionSimpleTest {
     private static final String LIBELLE_ACTION_SIMPLE = "tagada";
     final ActionSimple actions= new ActionSimple(LIBELLE_ACTION_SIMPLE);
     
-    @Test
-    void testValeurCoursSup0() {
+    // @Test
+    // void testValeurCoursSup0() {
         
-    }
+    // }
 
-    @Test
-    void testValeurDuCoursPasEgalA0(){
+    // @Test
+    // void testValeurDuCoursPasEgalA0(){
        
-    }
+    // }
 
     @Test
     void testLibelleIdentique(){
@@ -31,4 +33,26 @@ class ActionSimpleTest {
         assertEquals(0.01, ActionSimple.getDefaultActionValue(), 0.0);
     }
 
+    @Test
+    void testConstructorWithCorrectParametersShouldWork() {
+        Assertions.assertDoesNotThrow(()->{
+            new ActionSimple(LIBELLE_ACTION_SIMPLE);
+        });
+    }
+
+    @Test
+    void testMapCours() {
+        ActionSimple action = new ActionSimple("Test Action");
+
+        action.getMapCours().put(new Jour(2025, 1), 100.0);
+        action.getMapCours().put(new Jour(2025, 2), 105.0);
+
+        assertEquals(2, action.getMapCours().size(), "La taille de la mapCours devrait être 2");
+        assertTrue(action.getMapCours().containsKey(new Jour(2025, 1)), "La mapCours devrait contenir la clé Jour(2025, 1)");
+        assertTrue(action.getMapCours().containsKey(new Jour(2025, 2)), "La mapCours devrait contenir la clé Jour(2025, 2)");
+        assertFalse(action.getMapCours().containsKey(new Jour(2025, 3)), "La mapCours ne devrait pas contenir la clé Jour(2025, 3)");
+        assertEquals(100.0, action.getMapCours().get(new Jour(2025, 1)), "La valeur associée à Jour(2025, 1) devrait être 100.0");
+        assertEquals(105.0, action.getMapCours().get(new Jour(2025, 2)), "La valeur associée à Jour(2025, 2) devrait être 105.0");
+    }
+    
 }
