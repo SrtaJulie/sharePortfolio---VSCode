b7553cfb3c35bc8f4aea11055f1593ad27748a61
craftlistl <adrienlistl@gmail.com>
2025-05-20 15:09:45 +0200

    feat: setter tests

--- src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
+++ src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 class PortfeuilleTest {
 
+    private static final String ACTION_LIBELLE = "actionLibelle";
+
     @Test
     void testNewPortfeuille(){
         assertDoesNotThrow(() -> {
@@ -23,6 +25,22 @@ class PortfeuilleTest {
         List<Action> result = p.getActions();
 
         ArrayList<Action> expected = new ArrayList<>();
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testPortefeuilleSetter(){
+        Portfeuille p = new Portfeuille();
+        Action action = new ActionSimple(ACTION_LIBELLE);
+
+        ArrayList<Action> expected = new ArrayList<>();
+
+        expected.add(action);
+
+        p.setActions(expected);
+
+        List<Action> result = p.getActions();
 
         assertEquals(expected, result);
     }
