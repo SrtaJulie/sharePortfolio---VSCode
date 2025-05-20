156d1b1b3fa7f58678e5cb6af1feb4bbd19c2735
craftmanlistl <95369131+craftlistl@users.noreply.github.com>
2025-05-20 15:28:19 +0200

    1 us créer un portefeuille dactions (#34)
    * feat: class portfeuille & test de la creation portfeuille
    
    * feat: test portefeuille is empty by default
    
    * fix: ajouter aux gitignore des dossier Intellij Idea
    
    * fix: ajouter aux gitignore des dossier vscode
    
    * feat: setter tests
    
    ---------
    
    Co-authored-by: Bastien Bories <94696044+Bastien8131@users.noreply.github.com>

---
+++ src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2025 Adrien Listl Bastien Bories;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.utc.miage.shares;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class PortfeuilleTest {
+
+    private static final String ACTION_LIBELLE = "actionLibelle";
+
+    @Test
+    void testNewPortfeuille(){
+        assertDoesNotThrow(() -> {
+            Portfeuille p = new Portfeuille();
+        });
+    }
+
+    @Test
+    void testNewPortfeuilleIsEmpty(){
+        Portfeuille p = new Portfeuille();
+
+        List<Action> result = p.getActions();
+
+        ArrayList<Action> expected = new ArrayList<>();
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testPortefeuilleSetter(){
+        Portfeuille p = new Portfeuille();
+        Action action = new ActionSimple(ACTION_LIBELLE);
+
+        ArrayList<Action> expected = new ArrayList<>();
+
+        expected.add(action);
+
+        p.setActions(expected);
+
+        List<Action> result = p.getActions();
+
+        assertEquals(expected, result);
+    }
+
+}
