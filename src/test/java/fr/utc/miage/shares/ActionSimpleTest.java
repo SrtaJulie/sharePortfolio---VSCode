07990844f2390fea8f1f96ede60d845795042411
henry <chloehenry1.ch@gmail.com>
2025-05-20 16:31:18 +0200

    Permiers tests pour la classe ActionSimple [US]: Création d’action simple #14

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -1,14 +1,34 @@
 package fr.utc.miage.shares;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ActionSimpleTest {
+class ActionSimpleTest {
 
-    private static final double VALEUR_MINIMAL_COURS = 0;
+    private static final double VALEUR_MINIMAL_COURS = 0.01;
     private static final String LIBELLE_ACTION_SIMPLE = "tagada";
+    final ActionSimple actions= new ActionSimple(LIBELLE_ACTION_SIMPLE);
+    
     @Test
     void testValeurCoursSup0() {
-        final ActionSimple actions= new ActionSimple(LIBELLE_ACTION_SIMPLE);
+        
     }
+
+    @Test
+    void testValeurDuCoursPasEgalA0(){
+       
+    }
+
+    @Test
+    void testLibelleIdentique(){
+        Assertions.assertEquals(actions.getLibelle(), LIBELLE_ACTION_SIMPLE);
+    }
+
+    @Test
+    void testDefaultActionValue() {
+        assertEquals(0.01, ActionSimple.getDefaultActionValue(), 0.0);
+    }
+
 }
