07e1b49f6b902a06acc71574bff525f75bcd57aa
craftlistl <adrienlistl@gmail.com>
2025-05-20 15:00:50 +0200

    feat: test portefeuille is empty by default

--- src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
+++ src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
@@ -1,15 +1,30 @@
 package fr.utc.miage.shares;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class PortfeuilleTest {
 
     @Test
-    void testNewEmptyPortfeuille(){
+    void testNewPortfeuille(){
         assertDoesNotThrow(() -> {
             Portfeuille p = new Portfeuille();
         });
+    }
+
+    @Test
+    void testNewPortfeuilleIsEmpty(){
+        Portfeuille p = new Portfeuille();
+
+        List<Action> result = p.getActions();
+
+        ArrayList<Action> expected = new ArrayList<>();
+
+        assertEquals(expected, result);
     }
 
 }
