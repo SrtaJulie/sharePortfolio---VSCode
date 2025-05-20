68a08c0f4342c1d38accad7fb082e0b7486ee6d8
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-05-20 16:06:57 +0200

    feat: test de l'id client & nom/prenom

--- src/test/java/fr/utc/miage/shares/ClientTest.java
+++ src/test/java/fr/utc/miage/shares/ClientTest.java
@@ -1,12 +1,41 @@
 package fr.utc.miage.shares;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class ClientTest {
-    @Test
-    @Disabled
-    void testGetId() {
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class ClientTest {
+
+    public static final String PRENOM = "jean";
+    public static final String NOM = "michel";
+
+    @Test
+    void testCheckClientID() {
+        Client c1 = new Client(NOM, new Portfeuille(), PRENOM);
+        Client c2 = new Client(NOM, new Portfeuille(), PRENOM);
+
+       assertEquals(1, c1.getId());
+       assertEquals(2, c2.getId());
     }
+
+    @Test
+    void testCheckClientNomEtPrenom() {
+        Client c = new Client(NOM, new Portfeuille(), PRENOM);
+        assertAll("Test nom et prenom",
+            () -> {
+                String result = c.getNom();
+                assertEquals(NOM, result);
+            },
+            () -> {
+                String result = c.getPrenom();
+                assertEquals(PRENOM, result);
+            }
+        );
+    }
+
+    
 }
+
