d35dc37118326fe25c001d4d9397e94cdd17e16a
craftlistl <adrienlistl@gmail.com>
2025-05-20 15:49:56 +0200

    feat: client

---
+++ src/test/java/fr/utc/miage/shares/ClientTest.java
@@ -0,0 +1,12 @@
+package fr.utc.miage.shares;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class ClientTest {
+    @Test
+    @Disabled
+    void testGetId() {
+
+    }
+}
