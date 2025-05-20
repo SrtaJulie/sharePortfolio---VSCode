c92b37515ff2f81848fed60cedf9fa24d29e686d
craftlistl <adrienlistl@gmail.com>
2025-05-20 16:49:04 +0200

    feat: test suppression portefeuille et setters #7

--- src/main/java/fr/utc/miage/shares/Portfeuille.java
+++ src/main/java/fr/utc/miage/shares/Portfeuille.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Adrien Listl Bastien Bories;.
+ * Copyright 2025 Team Dev ABCEJOY;.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 package fr.utc.miage.shares;
 
--- src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
+++ src/test/java/fr/utc/miage/shares/PortfeuilleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Adrien Listl Bastien Bories;.
+ * Copyright 2025 Team Dev ABCEJOY;.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
