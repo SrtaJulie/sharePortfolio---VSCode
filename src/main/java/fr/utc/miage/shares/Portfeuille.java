d2ec7ed5fe7d3fa9a50cdc148a827562ca6c8ee3
craftlistl <adrienlistl@gmail.com>
2025-05-23 10:44:22 +0200

    feat: javadoc Portefeuille #5

--- src/main/java/fr/utc/miage/shares/Portfeuille.java
+++ src/main/java/fr/utc/miage/shares/Portfeuille.java
@@ -13,36 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package fr.utc.miage.shares;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+/*
+ * Classe portefeuille
+ */
 public class Portfeuille {
 
     List<Action> actions;
 
+    /**
+     * Constructeur, construit un portefeuille vide
+     */
     public Portfeuille() {
         this.actions = new ArrayList<>();
     }
 
+    /**
+     * donne les actions du portfeuille
+     *
+     * @return actions
+     */
     public List<Action> getActions() {
         return actions;
     }
 
+    /**
+     * change les actions du portefeuille (setter) throw NullPointerException
+     *
+     * @param actions List<Action>
+     */
     public void setActions(List<Action> actions) {
         this.actions = actions;
     }
 
+    /**
+     * Ajoute une action throw NullPointerException si action est null
+     *
+     * @param action
+     */
     public void ajoutAction(ActionSimple action) {
         if (action == null) {
             throw new NullPointerException();
         }
         this.actions.add(action);
     }
-
 
 }
